46254d3ee1095dfbcf58e1064f021de6ea853b1d
3lLobo <flocwolf@gmail.com>
2022-05-02 15:55:06 +0200

    First API 🍏

--- components/Charts/BarChart.js
+++ components/Charts/BarChart.js
@@ -1,30 +1,52 @@
-import React, { Component } from "react";
+import React from "react";
 import Card from "../Card/Card";
 // import Chart from "react-apexcharts";
 import dynamic from 'next/dynamic';
-import { barChartData, barChartOptions } from "../../variables/charts";
+// import { barChartData, barChartOptions } from "../../variables/charts";
+import { useEffect, useState } from "react";
+import { Box } from "@chakra-ui/react";
 
 
 const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
 
 function BarChart(props) {
 
+  const [data, setData] = useState()
+  const [options, setOptions] = useState()
+
+  useEffect(() => {
+    async function fetchApi() {
+      const res = await (await fetch("/api/charts")).json()
+      console.log(res)
+      setData(res.barChartData)
+      setOptions(res.barChartOptions)
+    }
+    fetchApi()
+  }, [])
+
   return (
-    <Card
-      py="1rem"
-      height={{ sm: "200px" }}
-      width="100%"
-      bg="linear-gradient(81.62deg, #313860 2.25%, #151928 79.87%)"
-      position="relative"
-    >
-      <Chart
-        options={barChartOptions}
-        series={barChartData}
-        type="bar"
-        width="100%"
-        height="100%"
-      />
-    </Card>
+    <Box>
+      { data ?
+      (
+        <Card
+          py="1rem"
+          height={{ sm: "200px" }}
+          width="100%"
+          bg="linear-gradient(81.62deg, #313860 2.25%, #151928 79.87%)"
+          position="relative"
+        >
+          <Chart
+            options={options}
+            series={data}
+            type="bar"
+            width="100%"
+            height="100%"
+          />
+        </Card>
+      )
+      : <Box />
+      }
+    </Box>
   );
 }
 
--- components/Charts/LineChart.js
+++ components/Charts/LineChart.js
@@ -1,34 +1,56 @@
 import React from "react";
 // import ReactApexChart from "react-apexcharts";
-import { lineChartData, lineChartOptions } from "../../variables/charts";
+// import { lineChartData, lineChartOptions } from "../../variables/charts";
 import dynamic from 'next/dynamic';
+import { useEffect, useState } from "react";
+import { Box } from "@chakra-ui/react";
 
 
 const ApexChart = dynamic(() => import('react-apexcharts'), { ssr: false });
 
 function LineChart() {
 
+  const [data, setData] = useState()
+  const [options, setOptions] = useState()
+
+  useEffect(() => {
+    async function fetchApi() {
+      const res = await (await fetch("/api/charts")).json()
+      setData(res.lineChartData)
+      setOptions(res.lineChartOptions)
+    }
+    fetchApi()
+  }, [])
+
+
   return (
-    <ApexChart
-      options={lineChartOptions}
-      series={lineChartData}
-      type="area"
-      width="100%"
-      height="100%"
-    />
+    <Box>
+      {data ?
+        (
+          <ApexChart
+            options={options}
+            series={data}
+            type="area"
+            width="100%"
+            height="100%"
+          />
+        )
+        : <Box />
+      }
+    </Box>
   );
 }
 
 // class LineChart extends React.Component {
 //   constructor(props) {
 //     super(props);
-    
+
 //     this.state = {
 //       chartData: [],
 //       chartOptions: {},
 //     };
 //   }
-  
+
 //   componentDidMount() {
 //     this.setState({
 //       chartData: lineChartData,
