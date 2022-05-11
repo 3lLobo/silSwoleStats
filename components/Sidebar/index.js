f7c6e2750bbc59819bbbfba1133e9488d304618d
3lLobo <flocwolf@gmail.com>
2022-05-11 18:21:36 +0200

    SVG update

--- components/Sidebar/index.js
+++ components/Sidebar/index.js
@@ -31,7 +31,7 @@ function Sidebar(props) {
     // SIDEBAR
     return (
         <Box ref={mainPanel}>
-            {true && (
+            {user && (
                 <Box display={{ sm: 'none', xl: 'block' }} position="fixed">
                     <Box
                         bg={sidebarBg}
